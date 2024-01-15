7b99f568f692e8a90bee46830405870e6f3a9dc6
shree-2802 <pattupadmashree@gmail.com>
2024-01-15 18:51:40 +0530

    blog card

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 import style from './Article.module.scss'
-const Article = () => {
+const Article = ({title,image}) => {
   return (
-    <div>Article</div>
+    <div className={style.gpt3__article}>
+      <div className={style.gpt3__article_container}>
+          <div >        
+            <img src={image} alt="blogImage" className={style.gpt3__article_container_image}/>
+            <div className={style.gpt3__article_container_text}>
+              <p>Sep 26, 2021</p>
+              <h3>{title}</h3>
+            </div>
+          </div>
+          <a href='#'>Read Full Article</a>
+      </div>
+    </div>
   )
 }
 
--- src/container/blog/Blog.jsx
+++ src/container/blog/Blog.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import {blogConstants} from '../../constants/constants';
 import style from './Blog.module.scss';
+import Article from '../../components/article/Article';
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <div className={style.gpt3__blog}>
+      <div className={style.gpt3__blog_container}>
+        <h3 className={`gradient__text ${style.gpt3__blog_container_h3}`}>{blogConstants.head}</h3>
+        <div className={style.gpt3__blog_container_cards}>
+          <div className={style.gpt3__blog_container_cards_A}>
+            <Article title={blogConstants.blogCard[0].title} image={blogConstants.blogCard[0].image}/>
+          </div>
+          <div className={style.gpt3__blog_container_cards_B}>
+            {
+              blogConstants.blogCard.map((item,index)=>{
+                return(
+                  index!==0 && <Article key={index} title={item.title} image={item.image}/>)
+              })
+            }
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
